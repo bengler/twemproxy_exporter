c9a377b62e3de002a0176f8ee31f6e328b5148ba
Erik Grinaker <erik@bengler.no>
2017-08-08 14:19:18 +0200

    Added web server.

--- twemproxy_exporter.go
+++ twemproxy_exporter.go
@@ -3,15 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
 )
 
 func main() {
 
 	var (
-		showVersion = flag.Bool("version", false, "Print version information.")
+		listenAddress = flag.String("web.listen-address", ":9284", "Address to listen on for web interface and telemetry.")
+		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+		showVersion   = flag.Bool("version", false, "Print version information.")
 	)
 	flag.Parse()
 
@@ -19,4 +24,19 @@ func main() {
 		fmt.Fprintln(os.Stdout, version.Print("twemproxy_exporter"))
 		os.Exit(0)
 	}
+
+	log.Infoln("Starting twemproxy_exporter", version.Info())
+
+	log.Infoln("Listening on", *listenAddress)
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+	<head><title>Twemproxy Exporter</title></head>
+	<body>
+		<h1>Twemproxy Exporter</h1>
+		<p><a href="` + *metricsPath + `">Metrics</a></p>
+	</body>
+</html>`))
+	})
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
 }
